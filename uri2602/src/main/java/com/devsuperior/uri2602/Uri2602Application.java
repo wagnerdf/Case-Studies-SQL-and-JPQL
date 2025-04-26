f99f1c61ed95372e575e1f2206a4851cd6a82f91
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2025-04-26 13:38:24 -0300

    URI 2602 Spring Boot JPQL

--- uri2602/src/main/java/com/devsuperior/uri2602/Uri2602Application.java
+++ uri2602/src/main/java/com/devsuperior/uri2602/Uri2602Application.java
@@ -25,12 +25,18 @@ public class Uri2602Application implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
-		List<CustomerMinProjection> list = repository.search1("RS");
+		List<CustomerMinProjection> list = repository.search1("rs");
 		List<CustomerMinDTO> result1 = list.stream().map(x -> new CustomerMinDTO(x)).collect(Collectors.toList());
 		
+		System.out.println("\n*** RESULTADO SQL RAIZ:");
 		for (CustomerMinDTO obj : result1) {
 			System.out.println(obj);
 		}
 		
+		List<CustomerMinDTO> result2 = repository.search2("rs");
+		System.out.println("\n*** RESULTADO JPQL:");
+		for (CustomerMinDTO obj : result2) {
+			System.out.println(obj);
+		}
 	}
 }
