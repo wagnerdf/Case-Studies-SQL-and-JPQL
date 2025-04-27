b20ba4664275af5fcbc500915eaf17f91a3a6537
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2025-04-27 12:28:48 -0300

    URI 2621 Spring Boot SQL e JPQL

--- uri2621/src/main/java/com/devsuperior/uri2621/Uri2621Application.java
+++ uri2621/src/main/java/com/devsuperior/uri2621/Uri2621Application.java
@@ -1,10 +1,15 @@
 package com.devsuperior.uri2621;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.devsuperior.uri2621.dto.ProductMinDTO;
+import com.devsuperior.uri2621.projections.ProductMinProjection;
 import com.devsuperior.uri2621.repositories.ProductRepository;
 
 @SpringBootApplication
@@ -19,6 +24,27 @@ public class Uri2621Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		List<ProductMinProjection> list = repository.search1(10, 20, "P");
+		List<ProductMinDTO> result1 = list.stream().map(x -> new ProductMinDTO(x)).collect(Collectors.toList());
 		
+		System.out.println("\n*** RESULTADO SQL RAIZ:");
+		for(ProductMinDTO obj : result1) {
+			System.out.println(obj);
+		}
+		System.out.println("\n\n");
+		List<ProductMinDTO> result2 = repository.search2(10, 20, "P");
+		
+		System.out.println("\n*** RESULTADO JPQL:");
+		for(ProductMinDTO obj : result2) {
+			System.out.println(obj);
+		}
 	}
 }
+
+
+
+
+
+
+
+
