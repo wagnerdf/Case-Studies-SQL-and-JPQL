66362bf3fba3983dda320d39fd510f7e724e2a8b
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2025-04-28 18:40:46 -0300

    URI 2609 Spring Boot SQL e JPQL

--- uri2609/src/main/java/com/devsuperior/uri2609/repositories/CategoryRepository.java
+++ uri2609/src/main/java/com/devsuperior/uri2609/repositories/CategoryRepository.java
@@ -1,9 +1,28 @@
 package com.devsuperior.uri2609.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.devsuperior.uri2609.dto.CategorySumDTO;
 import com.devsuperior.uri2609.entities.Category;
+import com.devsuperior.uri2609.projections.CategorySumProjection;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+	
+	@Query(nativeQuery = true, value = "SELECT c.name, SUM(p.amount) "
+			+ "FROM  products p "
+			+ "INNER JOIN categories c ON c.id = p.id_categories "
+			+ "GROUP BY c.name")
+	List<CategorySumProjection> search1();
+	
+	
+	@Query("SELECT new com.devsuperior.uri2609.dto.CategorySumDTO(obj.category.name, SUM(obj.amount)) "
+			+ "FROM  Product obj "
+			+ "GROUP BY obj.category.name")
+	List<CategorySumDTO> search2();
+	
+	
 
 }
